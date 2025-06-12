7e13723fd16b2396958091315e293048c433531c
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2025-06-12 18:32:03 -0300

    chore: add test config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
+// Vitest typing; only for TS, stripped out in emit
+// eslint-disable-next-line import/no-extraneous-dependencies
+import type { UserConfig as VitestUserConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'html'],
+    },
+  } as VitestUserConfig['test'],
   server: {
     host: true,
     port: 3000,
